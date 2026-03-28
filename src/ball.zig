4c813d980bfd098e1384d879233033c1edb4ab73
Nilanjan Mandal <nilanjanmandal15@gmail.com>
2026-03-28 16:19:10 +0530

    brick collision logic

--- src/ball.zig
+++ src/ball.zig
@@ -1,6 +1,8 @@
 const rl = @import("raylib");
+const std = @import("std");
 const constants = @import("constants.zig");
 const root = @import("root.zig");
+const brick = @import("brick.zig");
 
 pub const Ball = struct {
     x: f32,
@@ -45,9 +47,40 @@ pub fn update_ball(ball: *Ball) void {
 
 pub fn check_ball_collision(state: *root.State) void {
     const ball = &state.ball;
+    const ball_center = rl.Vector2{ .x = ball.x, .y = ball.y };
+
     const _paddle = state.paddle;
 
-    if (rl.checkCollisionCircleRec(rl.Vector2{ .x = ball.x, .y = ball.y }, constants.ball_radius, rl.Rectangle{ .x = _paddle.x, .y = _paddle.y, .width = constants.paddle_width, .height = constants.paddle_height })) {
+    var bricks_it = state.bricks.iterator();
+    var remove_key: ?brick.BrickKey = null;
+
+    while (bricks_it.next()) |brick_entry| {
+        const brick_val = brick_entry.value_ptr;
+        const collision = rl.checkCollisionCircleRec(ball_center, constants.ball_radius, rl.Rectangle{ .x = brick_val.x, .y = brick_val.y, .width = constants.brick_width, .height = constants.brick_height });
+
+        if (collision) {
+            const closest_x = std.math.clamp(ball_center.x, brick_val.x, brick_val.x + constants.brick_width);
+            const closest_y = std.math.clamp(ball_center.y, brick_val.y, brick_val.y + constants.brick_height);
+
+            const dx = ball_center.x - closest_x;
+            const dy = ball_center.y - closest_y;
+
+            if (@abs(dx) > @abs(dy)) {
+                ball.*.dx *= -1;
+            } else {
+                ball.*.dy *= -1;
+            }
+
+            remove_key = brick.BrickKey{ .x = @intFromFloat(brick_val.x), .y = @intFromFloat(brick_val.y) };
+            break;
+        }
+    }
+
+    if (remove_key) |key| {
+        _ = state.bricks.remove(key);
+    }
+
+    if (rl.checkCollisionCircleRec(ball_center, constants.ball_radius, rl.Rectangle{ .x = _paddle.x, .y = _paddle.y, .width = constants.paddle_width, .height = constants.paddle_height })) {
         ball.*.y = constants.height - constants.ball_radius - constants.paddle_height;
         ball.*.dy *= -1;
     }
