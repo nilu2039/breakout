3315081e8867b59cac73a7fc05c52c38c4ef8bdf
Nilanjan Mandal <nilanjanmandal15@gmail.com>
2026-04-02 01:01:23 +0530

    WASM support added.

--- src/root.zig
+++ src/root.zig
@@ -1,10 +1,24 @@
 const std = @import("std");
 const game = @import("game.zig");
+const builtin = @import("builtin");
 
 pub fn run() !void {
-    var gpa = std.heap.GeneralPurposeAllocator(.{}){};
-    const allocator = gpa.allocator();
-    defer _ = gpa.deinit();
+    const is_wasm = builtin.target.isWasiLibC() or builtin.target.os.tag == .emscripten;
+
+    var gpa: std.heap.GeneralPurposeAllocator(.{}) = undefined;
+
+    const allocator = blk: {
+        if (is_wasm) {
+            break :blk std.heap.c_allocator;
+        } else {
+            gpa = std.heap.GeneralPurposeAllocator(.{}){};
+            break :blk gpa.allocator();
+        }
+    };
+
+    defer {
+        if (!is_wasm) _ = gpa.deinit();
+    }
 
     var state = try game.State.init(allocator);
     defer state.deinit();
